2496af64f3027d371da184a5a6317267219ab30e
Catelijne W <24cwessels@pinewood.edu>
2022-06-26 15:42:03 -0700

    Initial Commit

---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by Cate Wessels on 6/26/22.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
